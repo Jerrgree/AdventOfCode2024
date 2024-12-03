4102664bdc8a1e4853d645a4edad9f729ccf05b3
Jerrgree <jeremyrgreenburg@gmail.com>
2024-12-02 23:38:16 -0600

    Add day 3 pt 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -4,7 +4,7 @@ fun main() {
 
     val input = readInput("Day03")
     println("Day 03 Part 1: ${day03Pt1(input)}")
-    //println("Day 03 Part 2: ${day03Pt2(input)}")
+    println("Day 03 Part 2: ${day03Pt2(input)}")
 }
 
 fun day03Pt1(input: List<String>): Int {
@@ -16,21 +16,61 @@ fun day03Pt1(input: List<String>): Int {
     return sum
 }
 
+fun day03Pt2(input: List<String>): Int {
+    var enabled = true
+    var sum = 0
+    for (line in input) {
+        val result = processEnhancedMulInstructions(line, enabled)
+
+        sum += result.first
+        enabled = result.second
+    }
+
+    return sum
+}
+
 fun processMulInstructions(input: String): Int {
     val matcher = "mul\\(\\d+,\\d+\\)".toRegex()
 
     val matches = matcher.findAll(input)
     var result = 0
     for (match in matches) {
-        val digitMatcher = "\\d+".toRegex()
-        val digits = digitMatcher.findAll(match.value)
-
-        val first = digits.first().value
-        val last = digits.last().value
-
-        result += (first.toInt() * last.toInt())
-
+        result += executeMult(match.value)
     }
 
     return result
+}
+
+fun processEnhancedMulInstructions(input: String, initialEnable: Boolean): Pair<Int, Boolean> {
+    var enable = initialEnable
+
+    val matcher = "mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)".toRegex()
+
+    val matches = matcher.findAll(input)
+    var result = 0
+    for (match in matches) {
+        var value = match.value
+        if (value == "do()")
+            enable = true
+        else if (value == "don't()")
+            enable = false
+        else {
+            if (!enable)
+                continue
+
+            result += executeMult(value)
+        }
+
+    }
+
+    return Pair(result, enable)
+}
+
+fun executeMult(input: String): Int {
+    val digitMatcher = "\\d+".toRegex()
+    val digits = digitMatcher.findAll(input)
+
+    val first = digits.first().value
+    val last = digits.last().value
+    return first.toInt() * last.toInt()
 }
