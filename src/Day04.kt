033daf3a8b650bee094892083084c73ff5af5d8d
Jerrgree <jeremyrgreenburg@gmail.com>
2024-12-04 00:23:42 -0600

    Add day 4 pt 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -4,7 +4,7 @@ fun main() {
 
     val input = readInput("Day04")
     println("Day 04 Part 1: ${day04Pt1(input)}")
-    //println("Day 04 Part 2: ${day04Pt2(input)}")
+    println("Day 04 Part 2: ${day04Pt2(input)}")
 }
 
 fun day04Pt1(input: List<String>): Int {
@@ -17,6 +17,22 @@ fun day04Pt1(input: List<String>): Int {
                 continue
 
             count += traverseGridSimple(input, j, i, height, width)
+        }
+    }
+
+    return count
+}
+
+fun day04Pt2(input: List<String>): Int {
+    val height = input.count() - 1
+    val width = input[0].length - 1
+    var count = 0
+    for (i in 0..height) {
+        for (j in 0..width) {
+            if (input[i][j] != 'A')
+                continue
+
+            count += xMas(input, j, i, height, width)
         }
     }
 
@@ -75,82 +91,17 @@ private fun traverseGridSimple(input: List<String>, x: Int, y: Int, width: Int, 
     return count
 }
 
-private fun traverseGrid(input: List<String>, x: Int, y: Int, width: Int, height: Int): Int {
-    val currentChar = input[y][x]
-    val nextExpectedChar = nextChar(currentChar)
-    var count = 0
+private fun xMas(input: List<String>, x: Int, y: Int, width: Int, height: Int): Int {
+    if (y - 1 < 0 || y + 1 > height ||
+        x - 1 < 0 || x + 1 > width
+    )
+        return 0
 
-    // top left
-    if (y != 0 && x != 0) {
-        val ny = y - 1
-        val nx = x - 1
-        count += checkSquare(input, nx, ny, width, height, nextExpectedChar)
-    }
-
-    // top center
-    if (y != 0) {
-        val ny = y - 1
-
-        count += checkSquare(input, x, ny, width, height, nextExpectedChar)
-    }
-    // top right
-    if (y != 0 && x != width) {
-        val ny = y - 1
-        val nx = x + 1
-        count += checkSquare(input, nx, ny, width, height, nextExpectedChar)
-    }
-
-    // left
-    if (x != 0) {
-        val nx = x - 1
-        count += checkSquare(input, nx, y, width, height, nextExpectedChar)
-    }
-
-    // right
-    if (x != width) {
-        val nx = x + 1
-        count += checkSquare(input, nx, y, width, height, nextExpectedChar)
-    }
-
-    // bot left
-    if (y != height && x != 0) {
-        val ny = y + 1
-        val nx = x - 1
-        count += checkSquare(input, nx, ny, width, height, nextExpectedChar)
-    }
-
-    // bot center
-    if (y != height) {
-        val ny = y + 1
-        count += checkSquare(input, x, ny, width, height, nextExpectedChar)
-    }
-
-    // bot right
-    if (y != height && x != width) {
-        val ny = y + 1
-        val nx = x + 1
-        count += checkSquare(input, nx, ny, width, height, nextExpectedChar)
-    }
-
-    return count
-}
-
-private fun checkSquare(input: List<String>, x: Int, y: Int, width: Int, height: Int, nextExpectedChar: Char): Int {
-    val nextChar = input[y][x]
-    if (nextChar == nextExpectedChar) {
-        return if (nextChar == 'S')
-            1
-        else
-            traverseGrid(input, x, y, width, height)
-    }
+    if ((input[y - 1][x - 1] == 'M' && input[y + 1][x + 1] == 'S'
+                || input[y - 1][x - 1] == 'S' && input[y + 1][x + 1] == 'M') &&
+        (input[y + 1][x - 1] == 'M' && input[y - 1][x + 1] == 'S'
+                || input[y + 1][x - 1] == 'S' && input[y - 1][x + 1] == 'M')
+    )
+        return 1
     return 0
-}
-
-private fun nextChar(current: Char) = when (current) {
-    'X' -> 'M'
-    'M' -> 'A'
-    'A' -> 'S'
-    else -> {
-        error("Invalid Character")
-    }
 }
