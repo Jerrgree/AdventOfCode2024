cad3318e98aa8e679a004f6ec21184833bea6fdb
Jerrgree <jeremyrgreenburg@gmail.com>
2024-12-04 23:58:44 -0600

    Add day 5 pt 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -3,15 +3,19 @@
 import kotlin.math.floor
 
 fun main() {
-
     val input = readInput("Day05")
     println("Day 05 Part 1: ${day05Pt1(input)}")
-    //println("Day 05 Part 2: ${day05Pt2(input)}")
+    println("Day 05 Part 2: ${day05Pt2(input)}")
 }
 
 fun day05Pt1(input: List<String>): Int {
     val (rules, index) = buildRules(input)
     return processUpdates(input, index, rules)
+}
+
+fun day05Pt2(input: List<String>): Int {
+    val (rules, index) = buildRules(input)
+    return fixTheBadOnes(input, index, rules)
 }
 
 private fun buildRules(input: List<String>): Pair<List<Pair<Int, Int>>, Int> {
@@ -61,4 +65,48 @@ private fun getMiddleNumber(input: List<Int>): Int {
     var index = floor(input.count() / 2.0).toInt()
 
     return input[index]
+}
+
+private fun fixTheBadOnes(input: List<String>, start: Int, rules: List<Pair<Int, Int>>): Int {
+    val end = input.count() - 1
+    var badOnes = mutableListOf<List<Int>>()
+    for (i in start..end) {
+        val next = input[i]
+        val values = next.split(',')
+            .toInt()
+        var passes = true
+        var seenValues = mutableListOf<Int>()
+
+        for (value in values) {
+            val applicableRules = rules.filter { r -> r.first == value }
+            if (applicableRules.any { r -> seenValues.any { v -> v == r.second } }) {
+                passes = false
+                break
+            }
+
+            seenValues.add(value)
+        }
+
+        if (!passes)
+            badOnes.add(values)
+    }
+
+    return processBadOnes(badOnes, rules)
+}
+
+private fun processBadOnes(input: List<List<Int>>, rules: List<Pair<Int, Int>>): Int {
+    var sum = 0
+    val comparer = Comparator { left: Int, right: Int ->
+
+        if (rules.filter { r -> r.first == left }
+                .any { r -> r.second == right })
+            return@Comparator -1
+        return@Comparator 1
+    }
+
+    for (value in input) {
+        sum += getMiddleNumber(value.sortedWith(comparer).toList())
+    }
+
+    return sum
 }
